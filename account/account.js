d1aa2716d256b7e6a87a7be2724ea5169e33927e
Kartik Yadav <kartikyadav94@gmail.com>
2021-03-12 16:21:12 +0530

    on registring takes to account page and fixed register issue if user already present

--- account/account.js
+++ account/account.js
@@ -3,7 +3,9 @@ var query = window.location.search;
 var url = new URLSearchParams(query);
 
 let uName = url.get("name");
+console.log(uName)
 var eMail = url.get("e-mail");
+console.log(eMail)
 
 let personName = document.getElementById("personName");
 personName.innerHTML = uName;
--- register/register.js
+++ register/register.js
@@ -40,14 +40,45 @@ function registerUser(e){
     let email = form.get("email");
     let password = form.get("password");
 
-    let user = new userData (firstName, lastName, email, password)
-    // console.log(user)
-    arr.push(user);
-    console.log(arr)
+    let userName = `${firstName} ${lastName}`;
 
-    arr = JSON.stringify(arr);
+    let usrData = localStorage.getItem("userData");
+    usrData = JSON.parse(usrData);
+    // console.log(usrData)
+    
+    let errMsg = document.getElementById("errMsg");
 
-    localStorage.setItem("userData", arr);
+    var flag =false;
+    for(var i = 0; i < usrData.length; i++){
+        if (firstName == usrData[i].firstName && lastName == usrData[i].lastName && email == usrData[i].email){
+            flag = true;
+            break;
+        }
+    }
+    if (flag == true){
+        errMsg.innerHTML = `This email address is already associated with an account. If this account is yours, you can <span>reset your password</span></li></ul>`
+    
+        var rstPass = document.querySelector("#errMsg span");
+        rstPass.style.color = "#777777";
+        rstPass.style.cursor = "pointer";
+        rstPass.addEventListener("click", ()=>{
+            location.href = `../lost Password/lostPassword.html`
+        })
+    }
+    else {
+        var url = `../account/account.html?name=${userName}&&e-mail=${email}`
+        location.href = url;
+
+        let user = new userData (firstName, lastName, email, password)
+        // console.log(user)
+        arr.push(user);
+        console.log(arr)
+
+        arr = JSON.stringify(arr);
+
+        localStorage.setItem("userData", arr);
+    }
+    
 
     let fName = document.getElementById("userFirstName");
     fName.value = null;
