b0303a4a83993a0c6069527dddee6ee7f9b9e815
Robin Singh <Rskamra002@gmail.com>
2021-03-13 10:55:49 +0530

    changed js

--- Payment/user_info.js
+++ Payment/user_info.js
@@ -1,16 +1,14 @@
 var pay = document.getElementById("pay-btn");
-
+var obj = {};
 function paymentPage() {
   var email = document.getElementById("email");
   var add = document.getElementById("u_address");
   var city = document.getElementById("u_city");
   var zip = document.getElementById("zipcode");
-  var obj = {
-    email: email.value,
-    adr: add.value,
-    city: city.value,
-    zip: zip.value,
-  };
+  obj.email = email.value;
+  obj.adr = add.value;
+  obj.city = city.value;
+  obj.zip = zip.value;
   localStorage.setItem("data", JSON.stringify(obj));
   location.assign("/payment.html");
 }
